4e8bc1292e28821e73a8589afe47ad1192f149e9
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-17 11:16:34 +0200

    Prepare app for push notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Push Notifications Client';
+
+  readonly VAPID_PUBLIC_KEY = "BLRQpb3GpmIRbkMXU1qymYQ3iQcnWm018Mbv_aCYKSRd1gsUlEmTjzaIJ-vNC1raPYvheLFUYc24HIKyIggxeOs";
+
+  title: string = "Push Notifications Client";
+  operation: string = "Subscribe";
+
 }
