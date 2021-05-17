84c8289b1ba7739d8b2b484fa9d046f5155dcced
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-17 16:14:07 +0200

    Remove subscription interfaces

--- src/app/interfaces/Keys.ts
+++
@@ -1,4 +0,0 @@
-export interface Keys {
-    p256dh: string;
-    auth: string;
-}
--- src/app/interfaces/PushSubscription.ts
+++
@@ -1,7 +0,0 @@
-import { Keys } from "./Keys";
-
-export interface PushSubscription {
-    endpoint: string;
-    expirationTime: number;
-    keys: Keys;
-}
